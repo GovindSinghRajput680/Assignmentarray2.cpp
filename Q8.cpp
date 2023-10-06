dd64d1e1da9140cec06a3f768ef959712c3167ca
GovindSinghRajput680 <145134257+GovindSinghRajput680@users.noreply.github.com>
2023-10-07 04:18:30 +0530

    Create Q8.cpp

---
+++ Q8.cpp
@@ -0,0 +1,3 @@
+Q.8: It will give an error because in the function getaverage
+we can't take integer first because we set 1st fornal parameter as an array.
+  So we can't pass 1st balance[0] = 1000.
